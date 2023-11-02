5b98730951a5e92c55a3709ed841f0b58b6f9ff7
Alex Sam <alsam3637@gmail.com>
2023-11-02 11:58:32 -0400

    Sorting posts.

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,11 +12,19 @@ class PostList extends Component
 {
     use WithPagination;
 
+    #[Url()]
+    public $sort = 'desc';
+
+    public function setSort($sort)
+    {
+        $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
+    }
+
     #[Computed]
     public function posts()
     {
         //return Post::published()->orderBy('published_at', 'desc')->SimplePaginate(3);
-        return Post::published()->orderBy('published_at', 'desc')->paginate(3);
+        return Post::published()->orderBy('published_at', $this->sort)->paginate(3);
     }
 
     public function render()
