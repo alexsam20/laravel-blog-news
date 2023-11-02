1aea411e1108c326fb543e169881ce6b642de622
Alex Sam <alsam3637@gmail.com>
2023-11-02 14:03:31 -0400

    Create component SearchBox. Implemented search by titles.

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -14,17 +15,27 @@ class PostList extends Component
 
     #[Url()]
     public $sort = 'desc';
+    public $search = '';
 
     public function setSort($sort)
     {
         $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
     }
 
+    #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
+    }
+
     #[Computed]
     public function posts()
     {
         //return Post::published()->orderBy('published_at', 'desc')->SimplePaginate(3);
-        return Post::published()->orderBy('published_at', $this->sort)->paginate(3);
+        return Post::published()
+            ->orderBy('published_at', $this->sort)
+            ->where('title', 'like', "%{$this->search}%")
+            ->paginate(3);
     }
 
     public function render()
