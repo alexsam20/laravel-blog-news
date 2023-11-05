dece9fb81a1a87d29286d880a748f4b931323542
Alex Sam <alsam3637@gmail.com>
2023-11-04 21:53:57 -0400

    Filter by Category.

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -5,6 +5,7 @@
         'gray' => 'text-gray-800',
         'blue' => 'text-blue-800',
         'red' => 'text-red-800',
+        'green' => 'text-green-800',
         'yellow' => 'text-yellow-800',
         default => 'text-gray-800',
     };
@@ -13,6 +14,7 @@
         'gray' => 'bg-gray-100',
         'blue' => 'bg-blue-100',
         'red' => 'bg-red-100',
+        'green' => 'bg-green-100',
         'yellow' => 'bg-yellow-100',
         default => 'bg-gray-100',
     };
